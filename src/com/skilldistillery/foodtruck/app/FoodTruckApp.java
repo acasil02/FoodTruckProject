f1d1e37408f28100473ccca28ed513bd911a3049
Angel <angel1179008@gmail.com>
2022-03-25 18:29:20 -0600

    Made starting code with a test meny, push to save

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -4,7 +4,27 @@ public class FoodTruckApp {
 
 	public static void main(String[] args) {
 		
+	FoodTruckApp foodTruckApp = new FoodTruckApp();
+	
+		
+		
+		
 		
 	}
 
+	
+	
+	
+	// NEEDS WORK
+	public  void displayMenu() {
+		System.out.println("---------------");
+		System.out.println("|    Menu     |");
+		System.out.println("| 1: Food Truck List     |");
+		System.out.println("| 2: Average Rated Food Trucks   |");
+		System.out.println("| 3: Highest Rated Food Trucks   |");
+		System.out.println("|    Quit   |");
+		System.out.println("---------------");
+	}
+
 }
+// this ones has menu, rating, display trucks
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -3,6 +3,62 @@ package com.skilldistillery.foodtruck.entities;
 public class FoodTruck {
 	private static int nextTruckId;
 	private String name;
+	private String foodType;
+	private double rating;
+	private int idGenerator = 1;
+	private int truckId;
+
 	
 	
+	public FoodTruck() {
+	}
+
+	public FoodTruck(String name, String foodType, double rating) {
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+		this.truckId = idGenerator;
+		
+	}
+
+	public static int getNextTruckId() {
+		return nextTruckId;
+	}
+
+	public static void setNextTruckId(int nextTruckId) {
+		FoodTruck.nextTruckId = nextTruckId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	
+	public String toString() {
+		return "FoodTruck [name = " + name + ", foodType = " + foodType +
+				", rating = " + rating + ", truckId = " + truckId
+				+ "]";
+	}
+
+	
+
 }
